891f1a15e6f358aeec6d4a0a2f2a9ec1f8777f28
ap514-cst <apomojumder688@gmail.com>
2026-08-15 22:35:09 +0600

    fix sitemap build issue

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteSitemap from 'vite-plugin-sitemap' // ✅ ডিফল্ট ইমপোর্ট (নামযুক্ত নয়)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
-    viteSitemap({
-      hostname: 'https://techcyfy.com', // আপনার ডোমেইন দিন
-      routes: [
-        '/',
-        '/about',
-        '/how-it-works',
-        '/vouchers',
-        '/reviews',
-        '/contact',
-        '/blog',
-        
-      ],
-      exclude: ['/admin', '/dashboard'], // বাদ দিতে চাইলে
-      outDir: 'dist',
-      changefreq: 'daily',
-      priority: 0.7,
-      lastmod: new Date().toISOString(),
-    })
-  ]
-})
+    sitemap({
+      generateRobotsTxt: false,
+    }),
+  ],
+});
